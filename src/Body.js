819c434e296a318a4d0cba62906c7ecc2b99a2a4
saurabh kumar <saurabhkumar9493@gmail.com>
2024-01-11 23:04:27 +0530

    hello

--- src/Body.js
+++ src/Body.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import RestrauntCard from './RestrauntCard'
+import './Body.css'
 function Body() {
   return (
-    <div>
+    <div className='restaurant-lists'>
+        <RestrauntCard/>
+        <RestrauntCard/>
+        <RestrauntCard/>
+        <RestrauntCard/>
+        <RestrauntCard/>
+        <RestrauntCard/>
+        <RestrauntCard/>
+        <RestrauntCard/>
+        <RestrauntCard/>
+        <RestrauntCard/>
+        <RestrauntCard/>
+        <RestrauntCard/>
+        <RestrauntCard/>
+        <RestrauntCard/>
+        <RestrauntCard/>
         <RestrauntCard/>
     </div>
   )
--- src/RestrauntCard.js
+++ src/RestrauntCard.js
@@ -1,12 +1,78 @@
 import React from 'react'
 import './RestrauntCard.css'
 function RestrauntCard() {
+  // const burgerKing = {
+  //   name: "Burger King",
+  //   image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/1ace5fa65eff3e1223feb696c956b38b",
+  //   cusines: ["Burger","American"],
+  //   rating: "4.2"
+  // }
+
+//config Driven UI
+// const config = [
+//   {
+//     type: "carousel",
+//     cards: [
+//       {
+//         offerName: "50% off"
+//       },
+//       {
+//         offerName: "No Delivery Charge"
+//       }
+//     ]
+//   },
+//   {
+//     type: "restutaurants",
+//     cards: [
+//       {
+//         name: "Burger King",
+//         image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/1ace5fa65eff3e1223feb696c956b38b",
+//         cusines: ["Burger","American"],
+//         rating: "4.2"
+//       },
+//       {
+//         name: "KFC",
+//         image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/1ace5fa65eff3e1223feb696c956b38b",
+//         cusines: ["Burger","American"],
+//         rating: "4.2"
+//       }
+//     ]
+//   }
+// ]
+
+
+  const restrautList = [
+  {
+    name: "Burger King",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/1ace5fa65eff3e1223feb696c956b38b",
+    cusines: ["Burger","American"],
+    rating: "4.2"
+  },
+  {
+    name: "Burger King",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/1ace5fa65eff3e1223feb696c956b38b",
+    cusines: ["Burger","American"],
+    rating: "4.2"
+  },
+  {
+    name: "Burger King",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/1ace5fa65eff3e1223feb696c956b38b",
+    cusines: ["Burger","American"],
+    rating: "4.2"
+  },
+  {
+    name: "Burger King",
+    image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/1ace5fa65eff3e1223feb696c956b38b",
+    cusines: ["Burger","American"],
+    rating: "4.2"
+  }
+]
   return (
     <div className='card'>
-        <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/1ace5fa65eff3e1223feb696c956b38b" alt="card" />
-        <h2>Burger King</h2>
-        <h3>Burgers, American</h3>
-        <h4>4.2 stars</h4>
+        <img src= {restrautList[0].image} />
+        <h2>{restrautList[0].name}</h2>
+        <h3>{restrautList[0].cusines}</h3>
+        <h4>{restrautList[0].rating} stars</h4>
     </div>
   )
 }
